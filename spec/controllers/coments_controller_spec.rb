71674c1c31df76e79b117e505c982af91503cde8
Cherry <cherryzh@thoughtworks.com>
2016-01-05 17:01:08 +1100

    fix undefined class name

--- spec/controllers/coments_controller_spec.rb
+++ spec/controllers/coments_controller_spec.rb
@@ -18,7 +18,7 @@ require 'rails_helper'
 # Message expectations are only used when there is no simpler way to specify
 # that an instance is receiving a specific message.
 
-RSpec.describe commentsController, type: :controller do
+RSpec.describe CommentsController, type: :controller do
 
   # This should return the minimal set of attributes required to create a valid
   # comment. As you add validations to comment, be sure to
