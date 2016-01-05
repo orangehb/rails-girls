0db857e72f725af670682c323b77afaa593b269c
Cherry <cherryzh@thoughtworks.com>
2016-01-05 17:03:38 +1100

    fix undefined class name

--- spec/routing/coments_routing_spec.rb
+++ spec/routing/coments_routing_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-RSpec.describe commentsController, type: :routing do
+RSpec.describe CommentsController, type: :routing do
   describe "routing" do
 
     it "routes to #index" do
